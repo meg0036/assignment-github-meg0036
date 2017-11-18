10941cdda4c29e9d0a395df6b553c9a575b59557
Mason <meg0036@auburn.edu>
2017-11-17 22:41:01 -0600

    Assignment 7

--- IndigoGirls/dispatch.py
+++ IndigoGirls/dispatch.py
@@ -1,6 +1,6 @@
 import json
 import random
-
+import swipe
 
 '''
 Created September 22, 2017
@@ -71,128 +71,7 @@ def dispatch(messageJson=None):
         resultDictionary[u"gameStatus"] = "underway"
         return resultDictionary
 
-    def merge(array):
-        score=0
-        n = len(array)
-        temp = []
-        for i in range(n):
-            if array[i] != 0:
-                temp.append(array[i])
-        i = 0
-        while i < len(temp) - 1:
-            if temp[i] == 0:
-                del temp[i]
-            else:
-                if temp[i] == temp[i + 1]:
-                    temp[i] += 1
-                    score += 2 ** temp[i]
-                    del temp[i + 1]
-                i += 1
-        m = n - len(temp)
-        for i in range(m):
-            temp.append(0)
-        for i in range(n):
-            array[i] = temp[i]
-        return score
 
-    def Swipe(messageDictionary):
-        resultDictionary = {}
-
-        resultDictionary[u"score"]=0
-        resultDictionary[u"board"]={}
-
-        column=messageDictionary[u"board"][u"columnCount"]
-        row=messageDictionary[u"board"][u"rowCount"]
-        resultDictionary[u"board"][u"columnCount"] = column
-        resultDictionary[u"board"][u"rowCount"] = row
-        length=column * row
-        resultDictionary[u"board"][u"grid"] = [0] * length
-
-        if (u"direction" not in messageDictionary):
-            return buildErrorString('missing direction')
-        elif messageDictionary[u"direction"] not in ['up','down','left','right']:
-            return buildErrorString('invalid direction')
-
-        if length!=len(messageDictionary[u"board"][u"grid"]):
-            return buildErrorString('invalid board')
-
-        temp_board = []
-        msg_grid = messageDictionary[u"board"][u"grid"]
-
-        if messageDictionary[u"direction"] == 'right':
-            for i in range(row):
-                temp_row = []
-                for j in range(column):
-                    temp_row.append(msg_grid[i * column + j])
-                temp_board.append(temp_row[::-1])
-
-            for i in range(row):
-                score = merge(temp_board[i])
-                resultDictionary[u"score"] += score
-                temp_board[i] = temp_board[i][::-1]
-            for i in range(row):
-                for j in range(column):
-                    resultDictionary[u"board"][u"grid"][i * column + j] = temp_board[i][j]
-
-        elif messageDictionary[u"direction"] == 'left':
-            for i in range(row):
-                temp_row = []
-                for j in range(column):
-                    temp_row.append(msg_grid[i * column + j])
-                temp_board.append(temp_row)
-
-            for i in range(row):
-                score = merge(temp_board[i])
-                resultDictionary[u"score"] += score
-            for i in range(row):
-                for j in range(column):
-                    resultDictionary[u"board"][u"grid"][i * column + j] = temp_board[i][j]
-
-        elif messageDictionary[u"direction"] == 'up':
-            for j in range(column):
-                temp_column = []
-                for i in range(row):
-                    temp_column.append(msg_grid[i * column + j])
-                temp_board.append(temp_column)
-
-            for i in range(row):
-                score = merge(temp_board[i])
-                resultDictionary[u"score"] += score
-
-            for j in range(column):
-                for i in range(row):
-                    resultDictionary[u"board"][u"grid"][i + j * row] = temp_board[i][j]
-
-        elif messageDictionary[u"direction"] == 'down':
-            for j in range(column):
-                temp_column = []
-                for i in range(row):
-                    temp_column.append(msg_grid[i * column + j])
-                temp_board.append(temp_column[::-1])
-
-            for i in range(row):
-                score = merge(temp_board[i])
-                resultDictionary[u"score"] += score
-                temp_board[i] = temp_board[i][::-1]
-
-            for j in range(column):
-                for i in range(row):
-                    resultDictionary[u"board"][u"grid"][i + j * row] = temp_board[i][j]
-
-        if resultDictionary[u"board"][u"grid"] == msg_grid:
-            return buildErrorString('no tiles can be shifted')
-
-        while True:
-            index = random.randint(0, length - 1)
-            if resultDictionary[u"board"][u"grid"][index] == 0:
-                if random.random() > 0.25:
-                    resultDictionary[u"board"][u"grid"][index] = 1
-                else:
-                    resultDictionary[u"board"][u"grid"][index] = 2
-                break
-
-        resultDictionary[u"gameStatus"] = "underway"
-        return resultDictionary
 
     #Validate JSONness of input be converting the string to an equivalent dictionary
     try:
@@ -215,7 +94,7 @@ def dispatch(messageJson=None):
     if(messageDictionary[u"op"] == u"initializeGame"):
         resultDictionary = InitializeGame(messageDictionary)
     elif(messageDictionary[u"op"] == u"swipe"):
-        resultDictionary = Swipe(messageDictionary)
+        resultDictionary = swipe.Swipe(messageDictionary)
     else:
         resultDictionary = buildErrorString('op is invalid')
 
